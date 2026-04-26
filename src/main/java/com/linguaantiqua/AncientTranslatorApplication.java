4f3aebc83db81bacdc29a6731de56a2ac3062203
Erdkash1 <erdkash1@gmail.com>
2026-04-25 21:58:30 -0500

    initial project setup

---
+++ src/main/java/com/linguaantiqua/AncientTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.linguaantiqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientTranslatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linguaantiqua/AncientTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linguaantiqua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
